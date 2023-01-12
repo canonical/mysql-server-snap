e9bf56f45d484e435f7fd79eaa034d1d33c23751
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-01-12 15:58:54 -0500

    Drop Root Privileges to Snap_daemon User (#2)
    * Initial implementation snap_daemon user
    
    * Add privilege dropping
    
    * Reconfigure file permissions and add layout mapping
    
    * Add comments to wrapper script

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -5,11 +5,22 @@ if [ ! -f "${CONFIG_DIR}/initialized" ]; then
         $SNAP/usr/sbin/mysqld --initialize
         echo "configuration folder ${CONFIG_DIR} does not exist."
         echo "copying default config to ${CONFIG_DIR}"
+
+        # Copy over mysql config file
         cp -r $SNAP/etc/mysql $SNAP_COMMON
-        sed -i "s:/var/run/mysqld/mysqld.sock:$SNAP_COMMON/mysqld.sock:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
-        sed -i "s:# sock:sock:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
-        sed -i "s:/var/log/mysql/error.log:$SNAP_COMMON/error.log:g" $SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
-        touch $CONFIG_DIR/initialized
+
+        # Set file permissions so snap_daemon can write to various files/folders
+        chown -R snap_daemon:root $CONFIG_DIR
+	chown -R snap_daemon:root $SNAP_DATA/var
+
+        # Replace default configuration options to work within the snap environment
+        sed -i "s:/var/run/mysqld/mysqld.sock:$CONFIG_DIR/mysqld.sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
+        sed -i "s:# sock:sock:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
+        sed -i "s: mysql: snap_daemon:g" $CONFIG_DIR/mysql.conf.d/mysqld.cnf
+
+        # Create file to indicate initialization has been completed
+	touch $CONFIG_DIR/initialized
 fi
 
-$SNAP/usr/sbin/mysqld --defaults-file=$SNAP_COMMON/mysql/mysql.conf.d/mysqld.cnf
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/sbin/mysqld --defaults-file=$CONFIG_DIR/mysql.conf.d/mysqld.cnf
